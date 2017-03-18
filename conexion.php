6c28d4fda147661eee066cd2e32474ca7d5a5061
Guillermo Ricardo Spindola Brisuela <gspindolab@gmail.com>
2017-03-17 22:09:05 -0600

    Hola

--- conexion.php
+++ conexion.php
@@ -6,6 +6,7 @@
         try {
             $conexion = new PDO("mysql:host=$host;dbname=$base", $user, $pass);
            //echo 'Conexion establecida';
+           //Hola Mundo
            $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exc) {
             echo "ERROR DE CONEXION  ".$exc->getMessage();
